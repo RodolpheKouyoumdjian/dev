99e0f371fd34ac9f9ccc91dada4d5b5756f04592
Rodolphe <kouyoumdjianrodolphe@gmail.com>
2024-12-15 10:20:05 +0000

    uh

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: "/out",
-    basePath: "/out",
+    assetPrefix: "/portfolio",
+    basePath: "/portfolio",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
